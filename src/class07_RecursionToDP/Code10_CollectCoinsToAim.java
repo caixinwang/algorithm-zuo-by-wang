e0da6017784ed7f2f11ce1b4cbed817764c52512
caixinwang <caixinwang2002@163.com>
2022-05-29 18:39:41 +0800

    10/11/12 commit

--- src/class07_RecursionToDP/Code10_CollectCoinsToAim.java
+++ src/class07_RecursionToDP/Code10_CollectCoinsToAim.java
@@ -2,7 +2,9 @@ package class07_RecursionToDP;
 
 public class Code10_CollectCoinsToAim {
 
-    /**每一层按照特点一种硬币（index）使用了几个来进行展开
+    /**
+     * 每一层按照特点一种硬币（index）使用了几个来进行展开
+     *
      * @param amount:硬币的面额
      * @param aim:目标
      * @return :返回的是最少的硬币数
@@ -32,6 +34,12 @@ public class Code10_CollectCoinsToAim {
         return min;
     }
 
+    /**
+     * 没有优化过的动态规划，里面有for循环
+     * @param amount
+     * @param aim
+     * @return
+     */
     private static int minCoinsDp1(int[] amount, int aim) {
         if (amount == null || amount.length == 0 || aim < 0) {
             return 0;
@@ -55,48 +63,11 @@ public class Code10_CollectCoinsToAim {
     }
 
     /**
-     * 用另一种递归过程来实现。每一层按照是否能够使用每一种硬币来展开，每层都只能使用一个
-     * @param amount:面额数组
-     * @param aim:要凑成的金额
+     * 优化过的动态规划，去掉了for循环
+     * @param amount
+     * @param aim
      * @return
      */
-    private static int minCoins2(int[] amount, int aim) {
-        if (amount == null || amount.length == 0 || aim < 0) {
-            return 0;
-        }
-        return process3(amount, aim);
-    }
-
-    private static int process3(int[] amount, int rest) {
-        if (rest==0){//顶层调用收集这些1
-            return 0;
-        }
-        if (!restIsOk(amount,rest)){
-            return -1;
-        }
-        int min=Integer.MAX_VALUE;
-        for (int coin:amount){
-            if (rest-coin>=0){
-                int next=process3(amount,rest-coin);
-                if (next==-1){
-                    continue;
-                }
-                min=Math.min(min,1+process3(amount,rest-coin));
-            }
-        }
-        return min==Integer.MAX_VALUE?-1:min;
-    }
-
-    private static boolean restIsOk(int[] amount ,int rest){//至少有一个硬币可以用
-        boolean ok=false;
-        for (int coin:amount){
-            if (rest>=coin){
-                ok=true;
-            }
-        }
-        return ok;
-    }
-
     private static int minCoinsDp2(int[] amount, int aim) {
         if (amount == null || amount.length == 0 || aim < 0) {
             return 0;
@@ -117,9 +88,80 @@ public class Code10_CollectCoinsToAim {
         return dp[0][aim];
     }
 
+    /**
+     * 用另一种递归过程来实现。每一层按照是否能够使用每一种硬币来展开，每层都只能使用一个.这种递归方式会导致调用得很深
+     * 会有非常非常多的重复计算，数据量大的话很可能跑不完。所以一定要改成动态规划。
+     *
+     * @param amount:面额数组
+     * @param aim:要凑成的金额
+     * @return
+     */
+    private static int minCoins2(int[] amount, int aim) {
+        if (amount == null || amount.length == 0 || aim < 0) {
+            return 0;
+        }
+        return process3(amount, aim);
+    }
+
+    private static int process3(int[] amount, int rest) {
+        if (rest == 0) {//顶层调用收集这些1
+            return 0;
+        }
+        if (!restIsOk(amount, rest)) {
+            return -1;
+        }
+        int min = Integer.MAX_VALUE;
+        for (int coin : amount) {
+            if (rest - coin >= 0) {//每一层选择性展开
+                int next = process3(amount, rest - coin);
+                if (next == -1) {
+                    continue;
+                }
+                min = Math.min(min, 1 + next);
+            }
+        }
+        return min == Integer.MAX_VALUE ? -1 : min;
+    }
+
+    private static boolean restIsOk(int[] amount, int rest) {//至少有一个硬币可以用
+        boolean ok = false;
+        for (int coin : amount) {
+            if (rest >= coin) {
+                ok = true;
+            }
+        }
+        return ok;
+    }
+
+    private static int minCoins2Dp(int[] amount, int aim) {
+        if (amount == null || amount.length == 0 || aim < 0) {
+            return 0;
+        }
+        int[] dp = new int[aim + 1];
+        dp[0] = 0;
+        for (int rest = 1; rest <= aim; rest++) {
+            if (!restIsOk(amount, rest)) {
+                dp[rest]= -1;
+                continue;
+            }
+            int min = Integer.MAX_VALUE;
+            for (int coin : amount) {
+                if (rest - coin >= 0) {
+                    int next = dp[rest - coin];
+                    if (next == -1) {
+                        continue;
+                    }
+                    min = Math.min(min, 1 + dp[rest - coin]);
+                }
+            }
+            dp[rest]= min == Integer.MAX_VALUE ? -1 : min;
+        }
+        return dp[aim];
+    }
+
+    //====================================================================================================================================
 
     /**
-     *
      * @param amount:面额数组，每种硬币可以使用无数个
      * @param aim:要凑成的目标
      * @return :凑成aim一共有多少种方法
@@ -171,6 +213,7 @@ public class Code10_CollectCoinsToAim {
 
     /**
      * 去for循环的方法：如果至少可以用一个硬币就可以使用规律。如果一个都不用就保留dp[index+1][rest]
+     *
      * @param amount:
      * @param aim:
      * @return
@@ -184,27 +227,56 @@ public class Code10_CollectCoinsToAim {
         dp[N][0] = 1;//第N行初始化完毕
         for (int index = N - 1; index >= 0; index--) {
             for (int rest = 0; rest <= aim; rest++) {
-                dp[index][rest]=dp[index+1][rest];
-                if (rest-amount[index]>=0){
-                    dp[index][rest]+=dp[index][rest-amount[index]];
+                dp[index][rest] = dp[index + 1][rest];
+                if (rest - amount[index] >= 0) {
+                    dp[index][rest] += dp[index][rest - amount[index]];
                 }
             }
         }
         return dp[0][aim];
     }
 
-
     public static void main(String[] args) {
-        int[] amount = {10,20,30};
-        int aim = 60;
-        System.out.println(minCoins(amount, aim));
-        System.out.println(minCoinsDp1(amount, aim));
-        System.out.println(minCoinsDp2(amount, aim));
-        System.out.println(minCoins2(amount, aim));
-        System.out.println("====================");
-        System.out.println(ways(amount,aim));
-        System.out.println(waysDp1(amount,aim));
-        System.out.println(waysDp2(amount,aim));
+        int[] amount = {2,3};
+        int aim = 100000;
+
+        long start = System.currentTimeMillis();
+//        System.out.println(minCoins(amount, aim));
+        long end = System.currentTimeMillis();
+//        System.out.println("cost time: " + (end - start) + "ms");
+
+        start = System.currentTimeMillis();
+//        System.out.println(minCoinsDp1(amount, aim));
+        end = System.currentTimeMillis();
+//        System.out.println("cost time: " + (end - start) + "ms");
+
+        start = System.currentTimeMillis();
+//        System.out.println(minCoinsDp2(amount, aim));
+        end = System.currentTimeMillis();
+//        System.out.println("cost time: " + (end - start) + "ms");
+
+        start = System.currentTimeMillis();
+//        System.out.println(minCoins2(amount, aim));
+        end = System.currentTimeMillis();
+//        System.out.println("cost time: " + (end - start) + "ms");
+
+        start = System.currentTimeMillis();
+        System.out.println(minCoins2Dp(amount, aim));
+        end = System.currentTimeMillis();
+        System.out.println("cost time: " + (end - start) + "ms");
+
+//        System.out.println("====================");
+//
+//        System.out.println(minCoins(amount, aim));
+//        System.out.println(minCoinsDp1(amount, aim));
+//        System.out.println(minCoinsDp2(amount, aim));
+//        System.out.println(minCoins2(amount, aim));
+//        System.out.println(minCoins2Dp(amount, aim));
+//        System.out.println("====================");
+//        System.out.println(ways(amount, aim));
+//        System.out.println(waysDp1(amount, aim));
+//        System.out.println(waysDp2(amount, aim));
+
     }
 
 }
---
+++ src/class07_RecursionToDP/Code12_LongestCommonSubsequence.java
@@ -0,0 +1,70 @@
+package class07_RecursionToDP;
+
+public class Code12_LongestCommonSubsequence {
+
+    private static int longestCommonSubsequence(String a, String b) {
+        if (a == null || b == null || a.length() == 0 || b.length() == 0) {
+            return 0;
+        }
+        char[] string1 = a.toCharArray();
+        char[] string2 = b.toCharArray();
+        return process1(string1, a.length() - 1, string2, b.length() - 1);
+    }
+
+    /**
+     * @param a:固定参数
+     * @param index1:0~index1 的字符串
+     * @param b:固定参数
+     * @param index2:0~index2 的字符串
+     * @return :返回a的0~index1的字符串和b的0~index2的字符串的最大公共子序列长度
+     */
+    private static int process1(char[] a, int index1, char[] b, int index2) {
+        if (index1 == 0 && index2 == 0) {
+            return a[index1] == b[index2] ? 1 : 0;
+        } else if (index1 == 0) {
+            return a[index1] == b[index2] ? 1 : process1(a, index1, b, index2 - 1);
+        } else if (index2 == 0) {
+            return a[index1] == b[index2] ? 1 : process1(a, index1 - 1, b, index2);
+        } else {//至少都有两个字符
+            //p1这里一定有一个前提条件就是a[index1]==b[index2]
+            int p1 = a[index1] == b[index2] ? process1(a, index1 - 1, b, index2 - 1) + 1 : 0;//最长公共子序列的最后一个字符都在a和b的最后
+            int p2 = process1(a, index1, b, index2 - 1);//最长公共子序列的最后一个字符在a不在b
+            int p3 = process1(a, index1 - 1, b, index2);//最长公共子序列的最后一个字符在b不在a
+            //p4这种情况可以不存在，因为p4一定小于p1,p2,p3.p2/p3的子串范围都大于p4，或者说p4已经包含在p2或者p3的决策中了。
+//            int p4=process1(a,index1-1,b,index2-1);
+            return Math.max(Math.max(p2, p3), p1);
+        }
+    }
+
+    private static int longestCommonSubsequence2(String str1, String str2) {
+        if (str1 == null || str2 == null || str1.length() == 0 || str2.length() == 0) {
+            return 0;
+        }
+        char[] a = str1.toCharArray();
+        char[] b = str2.toCharArray();
+        int[][] dp = new int[str1.length()][str2.length()];
+        dp[0][0] = a[0] == b[0] ? 1 : 0;
+        for (int index2 = 1; index2 < b.length; index2++) {
+            dp[0][index2] = a[0] == b[index2] ? 1 : dp[0][index2 - 1];
+        }
+        for (int index1 = 1; index1 < a.length; index1++) {
+            dp[index1][0] = a[index1] == b[0] ? 1 : dp[index1 - 1][0];
+        }
+        for (int index1 = 1; index1 < a.length; index1++) {
+            for (int index2 = 1; index2 < b.length; index2++) {
+                int p1 = a[index1] == b[index2] ? dp[index1 - 1][index2 - 1] + 1 : 0;
+                int p2 = dp[index1][index2 - 1];
+                int p3 = dp[index1 - 1][index2];
+                dp[index1][index2]= Math.max(Math.max(p2, p3), p1);
+            }
+        }
+        return dp[str1.length() - 1][str2.length() - 1];
+    }
+
+    public static void main(String[] args) {
+        String a = "123dsafKSJDFHKJSAHFKJSDAHFKJSDA";
+        String b = "ab123sadjhagsdHASDJKFHSJKADHF";
+//        System.out.println(longestCommonSubsequence(a, b));
+        System.out.println(longestCommonSubsequence2(a, b));
+    }
+}
